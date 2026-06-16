69cfa5e90ccb3f692939818834132c735af25b2f
TAUFIQ HASHMI <144479113+taufiqhashmi1@users.noreply.github.com>
2026-06-16 19:59:44 +0530

    switched to postgre

--- GroFul/GroFul/settings.py
+++ GroFul/GroFul/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'GroFul.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aforro_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgrespassword',
+        'HOST': 'db', # Important: Points to the Docker service name
+        'PORT': '5432',
     }
 }
 
